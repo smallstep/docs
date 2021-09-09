86fd83ab812907de1b74717345ade56f3067a04c
Alan Christopher Thomas <alan@smallstep.com>
2021-09-09 15:41:51 -0600

    Add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Heading, Paragraph } from '@smallstep/step-ui';
+
+const Page = () => (
+  <>
+    <Heading variant="h2">
+      Someone took a smallstep in the wrong direction.
+    </Heading>
+    <Paragraph>
+      Were you looking for our awesome how-tos on{' '}
+      <Link to="/practical-zero-trust">Pratical Zero Trust</Link>?
+    </Paragraph>
+  </>
+);
+
+export default Page;
