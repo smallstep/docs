2e18e27981f5fe6869887a3e8f4faeeb8655376b
Alan Christopher Thomas <alan@smallstep.com>
2021-10-01 16:25:58 -0600

    Allow explicit default block inclusion with MDXBlock

--- src/components/MDXBlock.jsx
+++ src/components/MDXBlock.jsx
@@ -4,23 +4,34 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import { DocContext } from '../context';
 
-const MDXBlock = ({ show, path }) => {
+const MDXBlock = (props) => {
+  const { show, path } = props;
+
   const { doc, provisioner, deployment, content } = useContext(DocContext);
+
+  let blockContent = content[`default/${path}`];
+
+  const docContent = content[`${doc.slug}/${path}`];
+  if (!props.default && docContent) {
+    blockContent = docContent;
+  }
 
   return (
     <div style={{ display: show ? 'block' : 'none' }}>
       <MDXRenderer doc={doc} page={{ provisioner, deployment }}>
-        {(content[`${doc.slug}/${path}`] || content[`default/${path}`]).body}
+        {blockContent.body}
       </MDXRenderer>
     </div>
   );
 };
 
 MDXBlock.propTypes = {
+  default: PropTypes.bool,
   show: PropTypes.bool,
 };
 
 MDXBlock.defaultProps = {
+  default: false,
   show: true,
 };
 
