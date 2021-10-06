429cba9603e6b699585cc744594433e7c373ed8c
Alan Christopher Thomas <alan@smallstep.com>
2021-10-06 11:11:51 -0600

    Add near-useless docs index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
-import { Heading, Paragraph } from '@smallstep/step-ui';
+import { Container, Heading, Paragraph } from '@smallstep/step-ui';
 
 const Page = ({ data }) => (
-  <>
+  <Container size="md">
+    <Heading variant="h2">Docs</Heading>
+    <Paragraph component="ul">
+      {data.allSitePage.edges.map(({ node }) => (
+        <li key={node.path}>
+          <Link to={node.path}>{node.context.frontmatter.title}</Link>
+        </li>
+      ))}
+    </Paragraph>
+
     <Heading variant="h2">Practical Zero Trust</Heading>
     <Paragraph component="ul">
       {data.allPztYaml.edges.map(({ node }) => (
@@ -12,11 +21,23 @@ const Page = ({ data }) => (
         </li>
       ))}
     </Paragraph>
-  </>
+  </Container>
 );
 
 export const query = graphql`
   query {
+    allSitePage(filter: { path: { regex: "^/docs/" } }) {
+      edges {
+        node {
+          path
+          context {
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
     allPztYaml {
       edges {
         node {
