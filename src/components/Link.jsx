c7c649e353bc24ee74c72dffeb4f5009f3d31b62
Alan Christopher Thomas <alan@smallstep.com>
2021-10-08 10:22:44 -0600

    Add hack to remove assetPrefix from internal / mdx links

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 import { Link as GatsbyLink } from 'gatsby';
 import { Link as SuiLink } from '@smallstep/step-ui';
 
-const Link = forwardRef(({ href, ...rest }, ref) =>
-  href.match(/^(https?|mailto):/) ? (
+const Link = forwardRef(({ href: originalHref, ...rest }, ref) => {
+  // this bit is a hack to overcome a bug in gatsby-plugin-mdx. for links in MDX
+  // that begin with a slash (/), gatsby prefixes them with the assetPrefix,
+  // which doesn't make sense. we make sure it gets stripped off.
+  const href = originalHref.replace(/^.*pzt\.infra\.smallstep\.com\//g, '');
+
+  return href.match(/^(https?|mailto):/) ? (
     <SuiLink ref={ref} external href={href} {...rest} />
   ) : (
     <SuiLink ref={ref} component={GatsbyLink} to={href} {...rest} />
-  )
-);
+  );
+});
 
 Link.propTypes = {
   href: PropTypes.string.isRequired,
