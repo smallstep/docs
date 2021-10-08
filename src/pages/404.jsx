5a4d5a5659e4f26e4a9d973f311d8de71e6d0544
Alan Christopher Thomas <alan@smallstep.com>
2021-10-08 15:32:26 -0600

    Prettier 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Heading, Paragraph } from '@smallstep/step-ui';
+import { Box } from '@material-ui/core';
+import { Container, Heading, Paragraph } from '@smallstep/step-ui';
 
 const Page = () => (
-  <>
-    <Heading variant="h2">
-      Someone took a smallstep in the wrong direction.
-    </Heading>
-    <Paragraph>
-      Were you looking for our awesome how-tos on{' '}
-      <Link to="/">Pratical Zero Trust</Link>?
-    </Paragraph>
-  </>
+  <Container size="md">
+    <Box p={8}>
+      <Heading variant="h2">
+        Someone took a smallstep in the wrong direction.
+      </Heading>
+      <Paragraph>
+        Maybe you'd like to see our <Link to="/docs">documentation</Link>?
+      </Paragraph>
+    </Box>
+  </Container>
 );
 
 export default Page;
