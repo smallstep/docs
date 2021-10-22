e81fb3b2d4fa1790350b5a847847da1c6e327f86
Alan Christopher Thomas <alan@smallstep.com>
2021-10-22 15:52:39 -0600

    Get rid of react iterator keys warning

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,7 @@ const ThirdPartyJS = require('@smallstep/internal').ThirdPartyJS;
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <ThirdPartyJS
+      key="head/third-party-js"
       clymPropertyId={process.env.GATSBY_CLYM_PROPERTY_ID}
       intercomAppId={process.env.GATSBY_INTERCOM_APP_ID}
     />,
