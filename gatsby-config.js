249651b596c70fa6b8878f466e983fa9ff5dbcdc
Alan Christopher Thomas <alan@smallstep.com>
2021-10-28 09:22:11 -0600

    Get rid of pzt.infra asset url

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: process.env.SITE_URL || `https://smallstep.com`,
   },
-  assetPrefix: process.env.ASSET_PREFIX || 'https://pzt.infra.smallstep.com',
+  assetPrefix: process.env.ASSET_PREFIX || 'https://prof.infra.smallstep.com',
   plugins: [
     `gatsby-plugin-remove-trailing-slashes`,
     {
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -7,8 +7,7 @@ const Link = forwardRef(({ href: originalHref, ...rest }, ref) => {
   // this bit is a hack to overcome a bug in gatsby-plugin-mdx. for links in MDX
   // that begin with a slash (/), gatsby prefixes them with the assetPrefix,
   // which doesn't make sense. we make sure it gets stripped off.
-  let href = originalHref.replace(/^.*pzt\.infra\.smallstep\.com\//g, '/');
-  href = href.replace(/^.*prof\.infra\.smallstep\.com\//g, '/');
+  const href = href.replace(/^.*prof\.infra\.smallstep\.com\//g, '/');
 
   return href.match(/^(https?|mailto):/) ? (
     <SuiLink ref={ref} external href={href} {...rest} />
