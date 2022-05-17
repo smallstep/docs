9b18c1b10dcb10a80014c06e6a152a359689a778
TommyA56Code <tommyandersonpersonal56@gmail.com>
2022-05-17 15:03:23 -0500

    Search Bar text goes away when typing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,11 +7,22 @@ import { InstantSearch,SearchBox,Hits } from 'react-instantsearch-hooks-web';
 
 const searchClient = algoliasearch('01ZCGVJMMX', '66bcc13d348eac437d89c68e2d5a2a76');
 
+function Hit({ hit }) {
+  return (
+    <article>
+      <h1>{hit.firstname}</h1>
+    </article>
+  );
+}
 const ariaLabel = { 'aria-label': 'description' };
 function SearchBar(){
     return(
     <Box mb={2} >
-      <Input defaultValue="Search" inputProps={ariaLabel} />
+        <InstantSearch searchClient={searchClient} indexName="instant_search">
+            <Input placeholder="Search" inputProps={ariaLabel} />
+            <Hits hitComponent={Hit} />
+        </InstantSearch>
+      
       
     </Box>
 )
