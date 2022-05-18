64646995726d96cf4491f4aed4c13433d9656573
TommyA56Code <tommyandersonpersonal56@gmail.com>
2022-05-18 17:47:13 -0500

    Added algolia-queries.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
+require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
