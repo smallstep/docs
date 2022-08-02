33d95f103515d763a831ea608bbf19818c7ee554
Mason Cooper <masoncooper19@gmail.com>
2022-08-02 10:30:22 -0700

    Formatting

--- src/components/Search/search-result.jsx
+++ src/components/Search/search-result.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'gatsby';
 import { default as React } from 'react';
 import { Heading } from '@smallstep/step-ui';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   connectStateResults,
   connectHits,
