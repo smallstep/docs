c4caa67a854bfbf26b42449ab7bdedbf53682fd2
Mason Cooper <masoncooper19@gmail.com>
2022-08-09 13:54:55 -0700

    Clear Button Added

--- src/components/Search/search-box.jsx
+++ src/components/Search/search-box.jsx
@@ -4,7 +4,7 @@ import { TextField } from '@smallstep/step-ui';
 import { Box } from '@material-ui/core';
 
 export default connectSearchBox(
-  ({ refine, currentRefinement, onFocus, onInput }) => (
+  ({ refine, value, onFocus, onInput, InputProps}) => (
     <Box component="form" mr={3}>
       <TextField
         fullWidth
@@ -13,9 +13,10 @@ export default connectSearchBox(
         placeholder="Search"
         aria-label="Search"
         onChange={(e) => refine(e.target.value)}
-        value={currentRefinement}
+        value={value}
         onFocus={onFocus}
         onInput={onInput}
+        InputProps={InputProps}
       />
     </Box>
   )
